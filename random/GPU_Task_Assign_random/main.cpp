d6a2b702a5915442c61dfd20ca039ad706666ec4
Zhe Song <songzhe.ustc@gmail.com>
2013-12-11 22:30:35 -0800

    create random proj

---
+++ random/GPU_Task_Assign_random/main.cpp
@@ -0,0 +1,18 @@
+////
+////  main.cpp
+////  GPU_Task_Assign_random
+////
+////  Created by zhe-mac on 13-12-11.
+////  Copyright (c) 2013年 zhe-mac. All rights reserved.
+////
+//
+//#include <iostream>
+//
+//int main(int argc, const char * argv[])
+//{
+//
+//    // insert code here...
+//    std::cout << "Hello, World!\n";
+//    return 0;
+//}
+//
